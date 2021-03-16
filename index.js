d366c774c46fb0578583e85684382d714b1f2d26
Charles Grant Nichols <grant@inkdots.com>
2021-03-16 15:58:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards(names, surprise) {
+    let people = []
+
+
+    for (let i = 0; i < names.length; i++) {
+        
+       people.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+
+    }
+return people
+
+}
+
+
+
+function countDown() {
+    let i = 10;
+    while (i > 0) {
+        console.log(i);
+        i--;
+    }
+    console.log(i);
+}
